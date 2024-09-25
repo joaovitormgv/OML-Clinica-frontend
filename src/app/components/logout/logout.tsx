1004b4fe75bbe3b6b58eda4a63e99994a27b450f
joaovitormgv <joaovitormgv@alu.ufc.br>
2024-09-25 09:52:34 -0300

    fix:  add logout navBar

--- src/app/components/logout/logout.tsx
+++ src/app/components/logout/logout.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const LogoutButton: React.FC = () => {
+  const router = useRouter();
+
   const handleLogout = () => {
     localStorage.removeItem('userToken');
     localStorage.removeItem('userCargo');
     console.log("Logout successful");
+    router.push('/pages/cargo');
   };
 
   return (
     <button
       onClick={handleLogout}
-      className="mt-4 bg-red-500 text-white font-semibold px-6 py-2 rounded-lg hover:bg-red-600"
+      className="bg-red-500 text-white font-semibold px-6 py-2 rounded-lg hover:bg-red-600"
     >
       Logout
     </button>
