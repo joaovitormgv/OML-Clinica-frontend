59e1aba780a8d3580484ca03418738f27ecde222
joaovitormgv <joaovitormgv@alu.ufc.br>
2024-09-24 23:09:00 -0300

    feat: integrar login e salvar id no localstorage

--- src/app/pages/admin/login/page.tsx
+++ src/app/pages/admin/login/page.tsx
@@ -1,12 +1,37 @@
 "use client";
 import React from "react";
 import CustomForm from "@/app/components/form/customForm";
+import LogoutButton from "@/app/components/logout/logout";
 
 const AdminLoginPage = () => {
   // Função que será chamada ao submeter o formulário
-  const handleLoginSubmit = (formData: Record<string, string>) => {
-    console.log("Login data:", formData);
-    // Aqui você pode lidar com o envio dos dados de login
+  const handleLoginSubmit = async (formData: { email: string; nome: string }) => {
+    console.log("handleLoginSubmit called with:", formData);
+  
+    try {
+      const response = await fetch('http://localhost:8080/usuarios/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      console.log("HTTP status:", response.status);
+      const data = await response.json();
+      console.log("Response data:", data);
+  
+      if (response.ok) {
+        // Handle successful login
+        localStorage.setItem('userToken', data.id);
+        console.log("Login successful");
+      } else {
+        // Handle login error
+        console.error("Login failed:", data);
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
   };
 
   return (
@@ -23,13 +48,13 @@ const AdminLoginPage = () => {
         </div>
 
         {/* Right side (3/5) - Usando o CustomForm */}
-        <div className="w-3/5 p-8 flex flex-col justify-center items-center">
+        <div className="w-3/5 p-8 flex flex-col justify-center items-center text-black">
           {/* Chama o componente CustomForm */}
           <CustomForm
-            title="Cadastro"
+            title="Login Usuário"
             fields={[
               { label: "Email", name: "email", type: "text" },
-              { label: "Senha", name: "password", type: "password" },
+              { label: "Nome", name: "nome", type: "text" },
             ]}
             buttonText="Entrar"
             onSubmit={handleLoginSubmit}
