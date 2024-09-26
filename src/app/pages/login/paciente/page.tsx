ff542bf765a5726c350f6e186a92b96cc0a0e6e7
joaovitormgv <joaovitormgv@alu.ufc.br>
2024-09-26 18:36:40 -0300

    feat: add login paciente

--- src/app/pages/login/paciente/page.tsx
+++ src/app/pages/login/paciente/page.tsx
@@ -1,16 +1,53 @@
 "use client";
 import React from "react";
 import CustomForm from "@/app/components/form/customForm";
+import { useRouter } from "next/navigation";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const UserLoginPage = () => {
+  const router = useRouter();
   // Função que será chamada ao submeter o formulário
-  const handleLoginSubmit = (formData: Record<string, string>) => {
+  const handleLoginSubmit = async (formData: {email: string; nome: string}) => {
     console.log("Login data:", formData);
-    // Aqui você pode lidar com o envio dos dados de login
+
+    try {
+      const response = await fetch('http://localhost:8080/pacientes/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log("Response data:", data);
+
+      if (response.ok) {
+        // Handle successful login
+        toast.success("Login efetuado com sucesso");
+        localStorage.setItem('userToken', data.id);
+        localStorage.setItem('userCargo', data.cargo);
+        console.log("Login successful");
+        setTimeout(() => {
+          router.push('/pages/consultas/autorizados/calendario');
+        }, 1000);
+      } else {
+        // Handle login error
+        console.error("Login failed:", data);
+        throw new Error("Login failed");
+      }
+
+    } catch (error) {
+      toast.error("Erro ao fazer login: Usuário inválido");
+      console.error("Error during login:", error);
+    }
+
   };
 
   return (
     <div className="bg-[url('/assets/fundo.jpg')] bg-cover bg-center min-h-screen flex justify-center items-center">
+      <ToastContainer />
       {/* Overlay */}
       <div className="absolute inset-0 bg-customBlue opacity-60"></div>
 
@@ -28,8 +65,8 @@ const UserLoginPage = () => {
           <CustomForm
             title="Paciente"
             fields={[
-              { label: "CPF", name: "cpf", type: "text" },
-              { label: "Data de Nascimento", name: "birthDate", type: "text" },
+              { label: "E-mail", name: "email", type: "email" },
+              { label: "Data de Nascimento", name: "birthDate", type: "date" },
             ]}
             buttonText="Entrar"
             onSubmit={handleLoginSubmit}
